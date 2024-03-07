6d3b731542249b08ed48177cc81603c0359c6580
fgadda <facugadda@gmail.com>
2024-03-06 21:14:21 -0300

    add navbar

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/app/ui/navbar"
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-[28px] font-medium">Mercado Play</h1>
+      </main>
+    </>
+  )
+}
