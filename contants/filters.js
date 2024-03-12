7ac39c5666c492c8304af7ad259b956713aa045e
fgadda <facugadda@gmail.com>
2024-03-11 23:44:55 -0300

    create constants files

---
+++ contants/filters.js
@@ -0,0 +1,82 @@
+export const filters = {
+  peliculas: [
+    {
+      name: "Todos",
+      href: "/peliculas",
+    },
+    {
+      name: "Top 10",
+      href: "/peliculas/top-10",
+    },
+    {
+      name: "Películas Premiadas",
+      href: "/peliculas/peliculas-premiadas",
+    },
+    {
+      name: "Acción",
+      href: "/peliculas/accion",
+    },
+    {
+      name: "Animación",
+      href: "/peliculas/animacion",
+    },
+    {
+      name: "Aventura",
+      href: "/peliculas/aventura",
+    },
+    {
+      name: "Comedia",
+      href: "/peliculas/comedia",
+    },
+    {
+      name: "Crimen",
+      href: "/peliculas/crimen",
+    },
+    {
+      name: "Documental",
+      href: "/peliculas/documental",
+    },
+    {
+      name: "Drama",
+      href: "/peliculas/drama",
+    },
+    {
+      name: "Terror",
+      href: "/peliculas/terror",
+    },
+  ],
+  series: [
+    {
+      name: "Todos",
+      href: "/series",
+    },
+    {
+      name: "Top 10",
+      href: "/series/top-10",
+    },
+    {
+      name: "Drama",
+      href: "/series/drama",
+    },
+    {
+      name: "Comedia",
+      href: "/series/comedia",
+    },
+    {
+      name: "Anime",
+      href: "/series/anime",
+    },
+    {
+      name: "Acción & Aventura",
+      href: "/series/accion_aventura",
+    },
+    {
+      name: "Pasar el rato",
+      href: "/series/pasar_el_rato",
+    },
+    {
+      name: "Más generos",
+      href: "/series/mas_genero",
+    },
+  ],
+}
---
+++ contants/links.js
@@ -0,0 +1,120 @@
+export const links = [
+  { name: "Para vos", href: "/" },
+  { name: "Más vistos", href: "/filtrar/mas-vistos" },
+  {
+    name: "Películas",
+    href: "/filtrar/peliculas",
+  },
+  { name: "Series", href: "/filtrar/series" },
+  { name: "Infantil", href: "/filtrar/infantil" },
+]
+
+export const test = {
+  tabs: [
+    { name: "Para vos", href: "/" },
+    { name: "Más vistos", href: "/filtrar/mas-vistos" },
+    {
+      name: "Películas",
+      href: "/filtrar/peliculas",
+    },
+    { name: "Series", href: "/filtrar/series" },
+    { name: "Infantil", href: "/filtrar/infantil" },
+  ],
+  filters: [
+    {
+      name: "Todos",
+      href: "/peliculas",
+      ref: "peliculas",
+    },
+    {
+      name: "Top 10",
+      href: "/peliculas/top-10",
+      ref: "peliculas",
+    },
+    {
+      name: "Películas Premiadas",
+      href: "/peliculas/peliculas-premiadas",
+      ref: "peliculas",
+    },
+    {
+      name: "Acción",
+      href: "/peliculas/accion",
+      ref: "peliculas",
+    },
+    {
+      name: "Animación",
+      href: "/peliculas/animacion",
+      ref: "peliculas",
+    },
+    {
+      name: "Aventura",
+      href: "/peliculas/aventura",
+      ref: "peliculas",
+    },
+    {
+      name: "Comedia",
+      href: "/peliculas/comedia",
+      ref: "peliculas",
+    },
+    {
+      name: "Crimen",
+      href: "/peliculas/crimen",
+      ref: "peliculas",
+    },
+    {
+      name: "Documental",
+      href: "/peliculas/documental",
+      ref: "peliculas",
+    },
+    {
+      name: "Drama",
+      href: "/peliculas/drama",
+      ref: "peliculas",
+    },
+    {
+      name: "Terror",
+      href: "/peliculas/terror",
+      ref: "peliculas",
+    },
+    {
+      name: "Todos",
+      href: "/series",
+      ref: "series",
+    },
+    {
+      name: "Top 10",
+      href: "/series/top-10",
+      ref: "series",
+    },
+    {
+      name: "Drama",
+      href: "/series/drama",
+      ref: "series",
+    },
+    {
+      name: "Comedia",
+      href: "/series/comedia",
+      ref: "series",
+    },
+    {
+      name: "Anime",
+      href: "/series/anime",
+      ref: "series",
+    },
+    {
+      name: "Acción & Aventura",
+      href: "/series/accion_aventura",
+      ref: "series",
+    },
+    {
+      name: "Pasar el rato",
+      href: "/series/pasar_el_rato",
+      ref: "series",
+    },
+    {
+      name: "Más generos",
+      href: "/series/mas_genero",
+      ref: "series",
+    },
+  ],
+}
