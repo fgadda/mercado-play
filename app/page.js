5a1d4c06f4497e2db8cef9166b60bd706c079633
fgadda <facugadda@gmail.com>
2024-03-11 23:45:57 -0300

    update layout initial page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
-import CardContent from "@/components/cards/card-content"
+import CardWrapper from "@/components/cards/cards"
 import { LoadMore } from "@/components/cards/load-more"
 import { fetchFeedContent } from "@/lib/fetch-feed-content"
 import Layout from "@/app/filtrar/layout"
 
 export default async function Home() {
-  const { results } = await fetchFeedContent({ offset: 0 })
+  const { results } = await fetchFeedContent({ offset: 0, filter: "" })
 
   return (
     <Layout>
-      <div className="mx-auto my-8 max-w-[1308px] px-[54px]">
-        <CardContent results={results} />
+      <section className="mt-8">
+        <CardWrapper results={results} />
         <LoadMore />
-      </div>
+      </section>
     </Layout>
   )
 }
