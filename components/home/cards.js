bf4380103204a4d53eee20175f200d9186a80d2f
fgadda <facugadda@gmail.com>
2024-03-18 19:12:33 -0300

    add links to feed cards

--- components/home/cards.js
+++ components/home/cards.js
@@ -1,25 +1,28 @@
 import Image from "next/image"
+import Link from "next/link"
 
-function Card({ content: { title, thumbnail, tag, subtitle } }) {
+function Card({ content: { title, thumbnail, tag, subtitle, url } }) {
   return (
     <article className="rounded-b-md bg-white drop-shadow transition duration-200 ease-out hover:drop-shadow-lg">
-      <Image
-        className="rounded-t-md"
-        src={thumbnail}
-        width="500"
-        height="500"
-        alt={title}
-        priority
-      />
-      <div className="flex flex-col gap-2 px-4 py-5 leading-none">
-        <p className="text-lg font-semibold leading-5">{title}</p>
-        <p className="truncate text-xs">{subtitle}</p>
-        <div>
-          <p className="mt-1 inline-block rounded-sm bg-gray-100 px-1.5 py-0.5 text-xs font-semibold">
-            {tag}
-          </p>
+      <Link href={url}>
+        <Image
+          className="rounded-t-md"
+          src={thumbnail}
+          width="500"
+          height="500"
+          alt={title}
+          priority
+        />
+        <div className="flex flex-col gap-1.5 px-4 py-4 leading-none">
+          <p className="text-lg font-semibold leading-5">{title}</p>
+          <p className="truncate text-xs">{subtitle}</p>
+          <div>
+            <p className="mt-1 inline-block rounded-sm bg-gray-100 px-1.5 py-0.5 text-xs font-semibold">
+              {tag}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </article>
   )
 }
