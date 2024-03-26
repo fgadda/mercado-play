55573394f2ff84e127f87e2dc410ffd4c504bc55
fgadda <facugadda@gmail.com>
2024-03-26 20:44:05 -0300

    fix logo

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,10 +1,12 @@
 import Image from "next/image"
 
+import logo from "@/app/logo.webp"
+
 export default function Navbar() {
   return (
     <header className="box-content h-[48px] border-b-2 bg-[#ffe600]">
       <div className="container mx-auto flex h-full max-w-[1200px] items-center justify-between px-[10px]">
-        <Image src="/logo.webp" width="134" height="34" alt="logo" priority />
+        <Image src={logo} width="134" height="34" alt="logo" priority />
         <nav className="flex items-center gap-[22px]">
           <p className="cursor-not-allowed text-sm antialiased">Creá tu cuenta</p>
           <p className="cursor-not-allowed text-sm antialiased">Ingresá</p>
