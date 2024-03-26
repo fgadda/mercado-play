5fdf76e1b9a5a401f4444a912e126bd9aa46cd67
fgadda <facugadda@gmail.com>
2024-03-25 22:44:13 -0300

    add pb to layout

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       <body className={proximaNova.className}>
         <Navbar />
         <main className="relative min-h-screen">
-          <div className="mx-auto max-w-[1308px] px-[54px]">{children}</div>
+          <div className="mx-auto max-w-[1308px] px-[54px] pb-[32px]">{children}</div>
         </main>
         <Footer />
       </body>
