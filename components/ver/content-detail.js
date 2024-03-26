4fd5c3a85e70165052b845ba4cbbf76e3fb2c69d
fgadda <facugadda@gmail.com>
2024-03-26 18:24:10 -0300

    refactor api service and content detail

--- components/ver/content-detail.js
+++ components/ver/content-detail.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 
-import Modal from "@/components/modal/modal"
+import Modal from "@/components/modal"
 import Spinner from "@/components/spinner"
 import { fetchContentDetail } from "@/lib/api"
 
@@ -10,49 +10,46 @@ export default function ContentDetail({ contentId }) {
   const [modalIsOpen, setModalIsOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
 
-  const { components } = content || {}
-  const descriptionContent = components?.[0]?.[0]
-  const contentHasData = components?.length > 0
+  const { title, mainColumn, restColumns } = content || {}
+  const hasData = Object.keys(content).length > 0
 
   useEffect(() => {
-    if (modalIsOpen && !contentHasData) {
+    if (modalIsOpen && !hasData) {
       setIsLoading(true)
-      fetchContentDetail({ contentId })
+      fetchContentDetail(contentId)
         .then(setContent)
         .finally(() => setIsLoading(false))
     }
-  }, [modalIsOpen, contentHasData, contentId])
+  }, [modalIsOpen, hasData, contentId])
 
   return (
     <Modal open={modalIsOpen} onOpenChange={() => setModalIsOpen(!modalIsOpen)}>
       <Modal.Button className="me-auto font-semibold text-primary">Conocer más</Modal.Button>
 
-      <Modal.Content title={content?.title}>
-        {isLoading && !contentHasData ? (
+      <Modal.Content title={title}>
+        {isLoading && !hasData ? (
           <div className="flex h-[300px]">
             <Spinner variant="primary" className="m-auto" />
           </div>
         ) : (
           <div className="mt-7 flex gap-7">
             <div className="flex flex-1 flex-col">
-              <h3 className="text-xl font-semibold">{descriptionContent?.title?.props?.label}</h3>
-              <p className="mt-2 w-full leading-tight">
-                {descriptionContent?.components[0]?.props?.label}
-              </p>
+              <h3 className="text-xl font-semibold">{mainColumn?.title}</h3>
+              <p className="mt-2 w-full leading-tight">{mainColumn?.value}</p>
             </div>
             <div className="h-100 hidden border-r border-gray-200 md:block"></div>
-            {content?.components?.slice(1).map((item, index) => (
+            {restColumns?.map((itemGroup, index) => (
               <div
                 key={index}
                 className="flex flex-col gap-4"
                 style={{ flex: "0 0 calc(25% - 28.25px)" }}
               >
-                {item?.map((x, i) => (
-                  <div key={i}>
-                    <h3 className="text-sm font-semibold">{x?.title?.props?.label}</h3>
+                {itemGroup.map((section, sectionIndex) => (
+                  <div key={sectionIndex}>
+                    <h3 className="text-sm font-semibold">{section?.props?.title?.props?.label}</h3>
                     <div className="flex flex-col gap-2">
-                      {x.components.map((component, gi) => (
-                        <p className="text-sm" key={gi}>
+                      {section?.props?.components?.map((component, componentIndex) => (
+                        <p className="text-sm" key={componentIndex}>
                           {component.type === "tag" ? (
                             <span className="mt-1 rounded-sm bg-gray-100 px-1.5 py-0.5 text-xs font-semibold">
                               {component.props.label}
---
+++ constants/types.js
@@ -0,0 +1,3 @@
+export const MEDIA_TYPE = "media-card"
+export const HORIZONTAL_MEDIA_TYPE = "horizontal-media-card"
+export const CONTENT_DETAIL_TYPE = "content-detail-overlay"
