6e9a5a6fe5e47d7937fbd08242f8e502183655c8
fgadda <facugadda@gmail.com>
2024-03-27 10:31:20 -0300

    implement tag component on feed cards

--- components/home/card-wrapper.js
+++ components/home/card-wrapper.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
 
 import LoadMore from "@/components/home/load-more"
+import Tags from "@/components/tags"
 import { fetchFeedContent } from "@/lib/api"
 
-function Card({ content: { title, thumbnail, tag, subtitle, url } }) {
+function Card({ content: { title, thumbnail, tags, subtitle, url } }) {
   return (
     <article className="rounded-b-md bg-white drop-shadow transition duration-200 ease-out hover:drop-shadow-lg">
       <Link href={url}>
@@ -14,13 +15,11 @@ function Card({ content: { title, thumbnail, tag, subtitle, url } }) {
           height="500"
           alt={title}
         />
-        <div className="flex flex-col gap-1.5 px-4 py-4 leading-none">
+        <div className="flex flex-col gap-1.5 px-4 pb-5 pt-4 leading-none">
           <p className="text-lg font-semibold leading-5">{title}</p>
           <p className="truncate text-xs">{subtitle}</p>
-          <div>
-            <p className="mt-1 inline-block rounded-sm bg-gray-100 px-1.5 py-0.5 text-xs font-semibold">
-              {tag}
-            </p>
+          <div className="mt-1">
+            <Tags tags={tags} />
           </div>
         </div>
       </Link>
