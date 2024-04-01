c34110ca118673948139257455377b3a116809b7
fgadda <facugadda@gmail.com>
2024-04-01 19:11:40 -0300

    refactor filters comp

--- components/layout/header/filters.js
+++ components/layout/header/filters.js
@@ -1,17 +1,18 @@
 import Link from "next/link"
 
 import { cn } from "@/lib/utils"
-import { filters } from "@/constants/filters"
+import { links } from "@/constants/links"
 
-export default function Filters({ activeView, currentSlug }) {
+export default function Filters({ view, path }) {
+  const filters = links.filters[view]
   return (
     <section className="no-scrollbar flex w-full items-center gap-2 overflow-x-scroll">
-      {filters[activeView].map(route => {
-        const linkIsActive = `/filtrar${route.href}` === `/filtrar/${currentSlug}`
+      {filters.map(filter => {
+        const linkIsActive = filter.href === path
         return (
           <Link
-            key={route.name}
-            href={`/filtrar${route.href}`}
+            key={filter.name}
+            href={`/${filter.href}`}
             className={cn(
               "flex h-[32px] items-center text-nowrap rounded-3xl border border-gray-500/70 px-3 text-sm md:text-base",
               {
@@ -19,7 +20,7 @@ export default function Filters({ activeView, currentSlug }) {
               },
             )}
           >
-            {route.name}
+            {filter.name}
           </Link>
         )
       })}
