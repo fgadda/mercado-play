60069e546c630d21296c337265fa56d5a2f3e113
fgadda <facugadda@gmail.com>
2024-04-01 19:11:52 -0300

    refactor tabs comp

--- components/layout/header/tabs.js
+++ components/layout/header/tabs.js
@@ -1,28 +1,26 @@
-"use client"
-
 import Link from "next/link"
-import { usePathname } from "next/navigation"
 
 import { links } from "@/constants/links"
 import { cn } from "@/lib/utils"
 
-export default function Tabs() {
-  const pathname = usePathname()
+export default function Tabs({ path = "" }) {
   return (
     <div className="border-b border-gray-300/80 text-center text-sm font-semibold md:text-base">
       <div className="flex flex-wrap gap-[40px]">
-        {links.map(link => (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={cn("inline-block py-3 hover:text-primary md:py-2", {
-              "border-b-2 border-primary text-primary":
-                link.href === "/" ? pathname === link.href : pathname.includes(link.href),
-            })}
-          >
-            {link.name}
-          </Link>
-        ))}
+        {links.tabs.map(tab => {
+          const linkIsActive = tab.href === "" ? path === tab.href : path.includes(tab.href)
+          return (
+            <Link
+              key={tab.name}
+              href={`/${tab.href}`}
+              className={cn("inline-block py-3 hover:text-primary md:py-2", {
+                "border-b-2 border-primary text-primary": linkIsActive,
+              })}
+            >
+              {tab.name}
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
