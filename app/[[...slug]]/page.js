79247a467a5edf7f9ffb62d308c2cdb60b83d55c
fgadda <facugadda@gmail.com>
2024-04-02 15:16:36 -0300

    remove suspense from feed pages

--- app/[[...slug]]/page.js
+++ app/[[...slug]]/page.js
@@ -1,25 +1,17 @@
-import { Suspense } from "react"
-
-import Spinner from "@/components/spinner"
-import CardWrapper from "@/components/home/card-wrapper"
-
-const Loading = () => {
-  return (
-    <div className="flex">
-      <Spinner className="mx-auto" />
-    </div>
-  )
-}
+import Cards from "@/components/home/cards"
+import LoadMore from "@/components/home/load-more"
+import { fetchFeedContent } from "@/lib/api"
 
 export default async function Page({ params: { slug } }) {
   const [, view, filter] = slug || []
   const filterParam = filter ? [view, filter].join("/") : view
 
+  const { results, nextPage } = await fetchFeedContent({ filter: filterParam })
+
   return (
     <section>
-      <Suspense fallback={<Loading />}>
-        <CardWrapper filter={filterParam} />
-      </Suspense>
+      <Cards results={results} />
+      {nextPage && <LoadMore filter={filterParam} />}
     </section>
   )
 }
