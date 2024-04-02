1317810bf94f7402392e64d2e73d748d817bedd0
fgadda <facugadda@gmail.com>
2024-04-02 12:03:14 -0300

    update font import paths

--- fonts/index.js
+++ fonts/index.js
@@ -3,12 +3,12 @@ import localFont from "next/font/local"
 export const proximaNova = localFont({
   src: [
     {
-      path: "../fonts/Proxima-Nova-Regular.otf",
+      path: "./Proxima-Nova-Regular.otf",
       weight: "400",
       subsets: ["latin"],
     },
     {
-      path: "../fonts/Proxima-Nova-Semibold.otf",
+      path: "./Proxima-Nova-Semibold.otf",
       weight: "600",
       subsets: ["latin"],
     },
