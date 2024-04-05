50f73e0e29ad5e9e496368ce0b01f2aee9d713fb
fgadda <facugadda@gmail.com>
2024-04-05 18:04:19 -0300

    add key to feed content results

--- components/home/cards.js
+++ components/home/cards.js
@@ -32,7 +32,7 @@ export default function Cards({ results }) {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {results.map(result => (
-        <Card key={result.url} content={result} />
+        <Card key={result.key} content={result} />
       ))}
     </div>
   )
