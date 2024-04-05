9fec5230767506cf2235ed63116e4e516c735c08
fgadda <facugadda@gmail.com>
2024-04-04 23:25:47 -0300

    remove intersection-obs and use framer motion to detect element within viewport

--- components/home/load-more.js
+++ components/home/load-more.js
@@ -1,17 +1,17 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { useInView } from "react-intersection-observer"
+import { useEffect, useState, useRef } from "react"
+import { useInView } from "framer-motion"
 
 import Spinner from "@/components/spinner"
 import { fetchFeedContent } from "@/lib/api"
 import Cards from "./cards"
 
 export default function LoadMore({ filter }) {
+  const ref = useRef(null)
   const [feedContent, setFeedContent] = useState({ results: [], nextPage: {} })
   const [offset, setOffset] = useState(24)
-
-  const { ref, inView } = useInView()
+  const isInView = useInView(ref, { margin: "400px 0px" })
 
   useEffect(() => {
     const filterResults = results => {
@@ -30,12 +30,12 @@ export default function LoadMore({ filter }) {
       setFeedContent({ results: [...feedContent.results, ...filterResults(results)], nextPage })
     }
 
-    if (inView) {
+    if (isInView) {
       loadMoreResults(offset)
       setOffset(prevOffset => prevOffset + 24)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [inView])
+  }, [isInView])
 
   return (
     <>
